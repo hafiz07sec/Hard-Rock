ef7225524bb6ea7c48398e3a3127269537c7904e
Hafizur Rahman Masum <32071354+hafiz07sec@users.noreply.github.com>
2021-11-17 02:35:59 +0100

    update spinner

--- api.js
+++ api.js
@@ -1,11 +1,12 @@
-const searchSong = async() => {
+const searchSong = async () => {
     const searchText = document.getElementById("input-search").value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
     //load data
+    toggleSpinner();
     try {
-        const res = await fetch(url); 
-    const data = await res.json(); 
-    displaySongs(data.data);
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySongs(data.data);
     } catch (error) {
         displayErrorMessage('Something went wrong. Please try again later!!!');
     }
@@ -14,13 +15,15 @@ const searchSong = async() => {
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
-    songContainer.innerHTML=``;
+    songContainer.innerHTML = ``;
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = ``;
 
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
-        songDiv.innerHTML = 
-        `<div class="col-md-9">
+        songDiv.innerHTML =
+            `<div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
             <audio controls>
@@ -35,26 +38,36 @@ const displaySongs = songs => {
             '${song.title}')"  class="btn btn-success">Get Lyrics</button>
         </div>`;
         songContainer.appendChild(songDiv);
+        toggleSpinner();
     })
 }
 
-const getLyric = async(artist, title) =>{
+const getLyric = async (artist, title) => {
     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
     try {
         const res = await fetch(url);
         const data = await res.json();
-        displayLyrics(data.lyrics); 
+        displayLyrics(data.lyrics);
     } catch (error) {
         displayErrorMessage('Sorry! Failed to load lyrics, Please try again later!!!');
     }
 }
 
-const displayLyrics = lyrics =>{
+const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics');
-    lyricsDiv.innerText= lyrics; 
+    lyricsDiv.innerText = lyrics;
 }
 
-const displayErrorMessage = (error) =>{
-    const errorTag= document.getElementById('error-Message');
-    errorTag.innerText = error; 
+const displayErrorMessage = (error) => {
+    const errorTag = document.getElementById('error-Message');
+    errorTag.innerText = error;
+}
+
+const toggleSpinner = () => {
+    const spinner = document.getElementById("loading-spinner");
+    spinner.classList.toggle('d-none');
+    
+    const songs = document.getElementById("song-container");
+    songs.classList.toggle('d-none');
+
 }
